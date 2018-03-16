76faf2827452e30b8b196aab90e5df52dc835448
Trieste Desautels <triested@live.com>
2018-03-15 23:13:52 -0700

    Fix machine-specific ncurses segfault

--- tools.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * This file contains various types and helper functions used 
- * throughout the project
- * */
-#include<utility>
-
-enum Direction {RIGHT, UP, LEFT, DOWN};
-typedef std::pair<int, int> Coordinates;
